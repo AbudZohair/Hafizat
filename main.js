8c13163cdad6887baba0e54c4588cdfb88f9e6d7
Abud Zohair <abud.zohair@gmail.com>
2020-04-23 17:10:36 +0200

    handle click

--- main.js
+++ main.js
@@ -1,42 +1,34 @@
 //Burger Menu
 const burgerMenu = document.querySelector(".burger-menu");
-			const navMenu = document.querySelector("ul.nav-links");
+const navMenu = document.querySelector("ul.nav-links");
 
-			burgerMenu.addEventListener("click", () => {
-				burgerMenu.classList.toggle("active");
-				navMenu.classList.toggle("collapse");
-            });
-            
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  navMenu.classList.toggle("collapse");
+});
 
+const requestTrials = document.querySelector("#requestTrial");
+const chat = document.querySelector("#maximizeChat");
+const contactNavLink = document.querySelector("#contactUs");
+const requestTrialNow = document.querySelector("#request-trial-now");
+const contactAdvisor = document.querySelector("#contact-advisor");
 
+contactAdvisor.addEventListener("click", (e) => {
+  e.preventDefault();
+  chat.click();
+});
 
+contactNavLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  chat.click();
+});
 
+requestTrials.addEventListener("click", (e) => {
+  e.preventDefault();
+  chat.click();
+});
 
-const requestTrials = document.querySelector('#requestTrial')
-const chat  = document.querySelector('#bubble-image');
-const contactNavLink = document.querySelector('#contactUs')
-const requestTrialNow = document.querySelector('#request-trial-now')
-const contactAdvisor = document.querySelector('#contact-advisor')
-
-contactAdvisor.addEventListener('click',()=>{
-    chat.click()
-})
-
-contactNavLink.addEventListener('click', () =>{
-    chat.click()
-})
-
-
-
-
-requestTrials.addEventListener('click', (e)=>{
-    e.preventDefault();
-    chat.click()
-    
-})
-
-
-
-requestTrialNow.addEventListener('click', ()=>}
-chat.clik()
-)
+requestTrialNow.addEventListener("click", (e) => {
+  e.preventDefault();
+  chat.clik();
+});
